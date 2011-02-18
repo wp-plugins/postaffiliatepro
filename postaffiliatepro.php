f610c1382eba40eb66ce85c20b1d9bc07a3bf02b
jurajsim <jurajsim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-18 13:15:26 +0000

    ver. 1.1.1 fix
    git-svn-id: https://plugins.svn.wordpress.org/postaffiliatepro/trunk@347330 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postaffiliatepro.php
+++ postaffiliatepro.php
@@ -4,7 +4,7 @@
  Plugin URI: http://www.qualityunit.com/#
  Description: Plugin that enable user signup integration integration with Post Affiliate Pro
  Author: QualityUnit
- Version: 1.1.0
+ Version: 1.1.1
  Author URI: http://www.qualityunit.com
  License: GPL2
  */
